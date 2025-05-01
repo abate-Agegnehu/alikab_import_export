1b2d8b079d0e3c22ea9e5485c5d1a9c922b08a25
Abate <abateagegnehu574@gmail.com>
2025-05-01 14:30:01 +0300

    complete fotter component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import AboutMain from '../components/about/AboutMain'
-import ExportProducts from '../components/about/ExportProducts'
-import VisionAndMission from '../components/about/VisionAndMission'
-import Team from '../components/about/Team'
-import Award from '../components/about/Award'
+import React from "react";
+import AboutMain from "../components/about/AboutMain";
+import ExportProducts from "../components/about/ExportProducts";
+import VisionAndMission from "../components/about/VisionAndMission";
+import Team from "../components/about/Team";
+import Award from "../components/about/Award";
+import Footer from "../components/footer/Footer";
 
 const About = () => {
   return (
-    <div >
-        <AboutMain/>
-        <ExportProducts/>
-        <VisionAndMission/>
-        <Team/>
-        <Award/>
+    <div>
+      <AboutMain />
+      <ExportProducts />
+      <VisionAndMission />
+      <Team />
+      <Award />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
