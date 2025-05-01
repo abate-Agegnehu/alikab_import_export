adc63d54119b9fc99a167a97c3e35035dbf9618a
Abate <abateagegnehu574@gmail.com>
2025-05-01 18:48:32 +0300

    Complete carcko car component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import About from "./pages/About"
+import About from "./pages/About";
 import Header from "./components/header/Header";
+import Carcko from "./pages/Carcko";
 const App = () => {
   return (
     <div>
       <div>From home page</div>
-      <Header/>
-      <About/>
+      <Header />
+      {/* <About /> */}
+      <Carcko />
     </div>
   );
 };
