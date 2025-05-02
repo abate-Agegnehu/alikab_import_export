954274921f45d25583953c4c1b262f95c2cb20fc
Abate <abateagegnehu574@gmail.com>
2025-05-02 13:53:59 +0300

    complete contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactMain from "../components/contact/ContactMain";
+import Footer from "../components/footer/Footer";
+
+const Contact = () => {
+  return (
+    <div>
+      <ContactMain />
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
