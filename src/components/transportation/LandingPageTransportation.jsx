92780a45eca3ba31a2a946b8d03bade1287a1e07
Abate <abateagegnehu574@gmail.com>
2025-05-08 16:40:11 +0300

    update changes

---
+++ src/components/transportation/LandingPageTransportation.jsx
@@ -0,0 +1,11 @@
+import BriefInfo from "./BriefInfo";
+import MainLayout from "../main/MainLayout";
+const LandingPageTransportation = () => {
+  return (
+    <MainLayout>
+      <BriefInfo />
+    </MainLayout>
+  );
+};
+
+export default LandingPageTransportation;
